ff34ab7eabad2647e629c8bad76205cebca59c2b
iayanpahwa <iayanpahwa@gmail.com>
2016-12-20 23:36:45 +0530

    Added Javascript color palette from http://jscolor.com and modified setup.py REST API call

--- src/setup.py
+++ src/setup.py
@@ -13,22 +13,11 @@ def main():
     # Pass the template data into the template powrcycle.html and return it to the user
     return render_template('index.html', **templateData)
 
-@app.route("/onn/<state>")
-def action_onn(state):
-        if state == 'A':
-			return "Style A on lights activated\n"
+@app.route("/onn/<value>")
+def sendRGB(value):
+    return "Hello" + value
 
-        elif state == 'B':
-            return "Style B on lights activated\n"
-
-
-@app.route("/off/<state>")
-def action_off(state):
-        if state == 'A':
-            return "Style A on lights deactivated\n"
-
-        elif state == 'B':
-			return "Style B on lights deactivated\n\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
+
